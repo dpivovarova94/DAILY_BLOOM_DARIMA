ee5e5ce31e43fedc15ca0b23f48417956ebdff3b
Alexandra Saalfeld <alexandra.saalfeld@gmail.com>
2023-06-06 12:41:57 +0200

    stashing changes

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,17 @@
 class ChallengesController < ApplicationController
+
+  def new
+    # check if user logs in for first time of today
+    # generate a new keyword
+    # if user has already logged in once, link to show
+  end
+
+  def create
+    @challenge = Challenge.new
+    @challenge.save
+    redirect_to new_challenge_post_path
+  end
+
+  def show
+  end
 end
