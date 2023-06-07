cc476eb6ab53cb9b0e0a7108d6e61c2293a2f9d3
axel <axelsearagomez@gmail.com>
2023-06-07 14:41:23 +0200

    3rdpull

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+puts 'Cleaning database'
+
 User.destroy_all
 
 test_user = User.create!(
@@ -16,6 +20,7 @@ Category.create!(name: "Nature")
 Category.create!(name: "Art")
 Category.create!(name: "Animals")
 
+puts 'adding categories'
 
 Keyword.create!(
   name: "Flower",
@@ -45,4 +50,28 @@ Keyword.create!(
 Keyword.create!(
   name: "Cats",
   category: Category.find_by(name: "Animals")
+)
+
+
+
+# faking users with faker gem
+10.times do
+  user =User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    location: Faker::Address.city,
+    avatar_url: Faker::Avatar.image,
+    password: "123456"
+  )
+end
+puts "users created"
+
+Toni = User.create(
+  first_name: "Toni",
+  last_name: "Steitz",
+  email: "toni@mail.de",
+  location: "Berlin",
+  avatar_url: Faker::Avatar.image,
+  password: "123456"
 )
