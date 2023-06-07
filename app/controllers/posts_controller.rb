cd25f12e8b2899e96a7e989cafec0022e9e25429
Antonia Steitz <antoniasteitz@gmail.com>
2023-06-07 14:36:09 +0200

    Working on the new and create action and vie for medium choices

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      case @post.medium
+      when 'photo'
+        # Handle photo upload logic
+      when 'poem'
+        # Handle poem upload logic
+      when 'text'
+        # Handle text upload logic
+      when 'song'
+        # Handle song upload logic
+      end
+        # Redirect or render appropriate response
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:medium, :picture_url, :poem, :text, :song_url)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_130652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+ 
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
