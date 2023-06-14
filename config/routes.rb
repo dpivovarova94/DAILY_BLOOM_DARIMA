de34eebe11d577476efb16f3e6d28110659092fc
axel <axelsearagomez@gmail.com>
2023-06-14 10:10:20 +0200

    categories issue fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
+  # root to: "pages#home"
   root 'welcome#index'
 
   get 'dashboard', to: 'pages#dashboard'
-
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :challenges, only: [:show, :new, :create] do
     resources :posts, only: [:new, :create]
   end
 
-  # this is the modification:
-  resources :posts, only: [:index, :show], as: :feed
+  get 'posts', to: 'posts#index', as: :feed
+  get '/posts/:id', to: 'posts#show', as: 'post'
+
 
   get 'user_categories/new', to: 'user_categories#new'
   post 'user_categories', to: 'user_categories#create'
   get 'user/:id/edit', to: 'user_categories#edit', as: 'user'
   patch 'user_categories/:id', to: 'user_categories#update', as: 'update_user_category'
 
+
   devise_for :users, controllers: { registrations: "users/registrations" }
+
+
 end
