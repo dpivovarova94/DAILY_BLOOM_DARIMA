5cf806062d78762b087ae0ef181b1fde2d82650f
Darima Pivovarova <dpivovarova@gmail.com>
2023-08-30 16:29:16 +0200

    added styling for my feed and friends feed toggler

--- app/javascript/controllers/posts_toggle_controller.js
+++ app/javascript/controllers/posts_toggle_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 
 export default class extends Controller {
-  static targets = ["friendsPosts", "myPosts"]
+  static targets = ["friendsPosts", "myPosts", "myButton", "friendsButton"]
 
   connect() {
     console.log("Connected to search controller");
@@ -15,10 +15,16 @@ export default class extends Controller {
     this.myPostsTarget.classList.remove("d-none");
     this.friendsPostsTarget.classList.add("d-none");
 
+    this.myButtonTarget.classList.add("selected")
+    this.friendsButtonTarget.classList.remove("selected")
+
   }
   selectFriendsPosts() {
     this.myPostsTarget.classList.add("d-none");
     this.friendsPostsTarget.classList.remove("d-none");
+
+    this.myButtonTarget.classList.remove("selected")
+    this.friendsButtonTarget.classList.add("selected")
   }
 
 }
